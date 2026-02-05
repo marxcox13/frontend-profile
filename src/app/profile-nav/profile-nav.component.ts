eb5be09333ea95c75184af8bf73650c6ccbf12d4
Marcos mh <81183500+marxcox13@users.noreply.github.com>
2026-02-05 17:38:42 -0500

    update version

---
+++ src/app/profile-nav/profile-nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-nav.component.html',
+  styleUrl: './profile-nav.component.scss'
+})
+export class ProfileNavComponent {
+
+}
